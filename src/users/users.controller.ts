82828f86bfe821de791aece3078558b24167d9e3
Mariyan <marian.sokolov1@gmail.com>
2024-12-02 23:07:14 +0200

    made user avatar image update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import cloudinary from 'cloudinary.config';
+import { Readable } from 'stream';
+import { UserMainInfoDTO } from './users.dto';
+
+function bufferToStream(buffer: Buffer): Readable {
+  const stream = new Readable();
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+}
 
 @Controller('users')
 export class UsersController {
@@ -11,4 +22,54 @@ export class UsersController {
     const users = await this.usersService.findAll();
     return users;
   }
+
+  @Get(':id')
+  async getUserMainInfoById(@Param('id') id: string): Promise<UserMainInfoDTO> {
+    const user = await this.usersService.findById(id);
+    if (!user) {
+      throw new Error('User not found.');
+    }
+    return {
+      username: user.username,
+      avatarUrl: user.avatarUrl
+    };
+  }
+  
+  @Post('upload-avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadAvatar(
+    @UploadedFile() file: Express.Multer.File,
+    @Body('userId') userId: string,
+  ) {
+    if (!file) {
+      throw new Error('No file uploaded.');
+    }
+  
+    try {
+      const uploadStream = (options) =>
+        new Promise((resolve, reject) => {
+          const stream = cloudinary.uploader.upload_stream(options, (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          });
+  
+          bufferToStream(file.buffer).pipe(stream);
+        });
+  
+      const result: any = await uploadStream({});
+
+      if(result) {
+        await this.usersService.updateAvatar(userId, result.secure_url);
+        return {
+          avatarUrl: result.secure_url,
+        };
+      } else {
+        console.error('Cloudinary Upload Error');
+      }
+    } catch (error) {
+      console.error('Cloudinary Upload Error:', error);
+      throw error;
+    }
+  }
+  
 }
---
+++ src/users/users.dto.ts
@@ -0,0 +1,5 @@
+export class UserMainInfoDTO {
+    username: string;
+    avatarUrl: string;
+  }
+  
